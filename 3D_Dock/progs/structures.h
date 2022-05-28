5f6d5c43f53699cccc7911edaba8b96bcd9dacc7
Your Name <adiboy_3112@kgpian.iitkgp.ac.in>
2022-05-28 10:02:46 +0530

    started converting to cuda for the main programme loop

--- 3D_Dock/progs/structures.h
+++ 3D_Dock/progs/structures.h
@@ -33,6 +33,7 @@ Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 #include <time.h>
 #include <rfftw.h>
 
+
 /************/
 
 /* These values directly below may be altered, and the programs rebuilt */
