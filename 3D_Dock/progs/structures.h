2f7fc4dc821efb543a8b123fbec5a9b66158303e
Your Name <adiboy_3112@kgpian.iitkgp.ac.in>
2022-05-30 13:06:14 +0530

    Problem with datatype conversion from fftw to cufft

--- 3D_Dock/progs/structures.h
+++ 3D_Dock/progs/structures.h
@@ -31,7 +31,7 @@ Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 #include <math.h>
 #include <stdlib.h>
 #include <time.h>
-#include <rfftw.h>
+// #include <rfftw.h>
 
 /************/
 
